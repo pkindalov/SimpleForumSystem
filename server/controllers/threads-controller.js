75010092e85950cfebbd3791b28d449ba93783f4
pkindalov <panayotkindalov@gmail.com>
2017-06-10 02:30:13 +0300

    Added controller for the threads

---
+++ server/controllers/threads-controller.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+const Thread = mongoose.model('Thread')
+// const Answers = require('../data/Answers')
+// const errorHandler = require('../utilities/error-handler')
+const errorHandler = require('../utilities/error-handler')
+
+module.exports = {
+  addTreadGet: (req, res) => {
+    res.render('threads/add')
+  },
+
+  addTreadPost: (req, res) => {
+    let threadReq = req.body
+
+    // some validations here
+    if (threadReq.title.length === 0) {
+      res.locals.globalError = 'Title cannot be empty'
+      res.render('threads/add')
+      return
+    }
+
+    Thread
+            .create({
+              title: threadReq.title,
+              description: threadReq.description,
+              date: threadReq.date
+            })
+            .then(thread => {
+              res.redirect('threads/all')
+            })
+            .catch(err => {
+              let message = errorHandler.handleMongooseError(err)
+              res.locals.globalError = message
+              res.render('threads/add', threadReq)
+            })
+  }
+
+}
