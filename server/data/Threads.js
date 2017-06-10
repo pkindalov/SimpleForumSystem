c79e291c30e2527babea3ff8c25725216750310a
pkindalov <panayotkindalov@gmail.com>
2017-06-10 03:09:04 +0300

    Fixed a little error in model Threads.js

--- server/data/Threads.js
+++ server/data/Threads.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 const ObjectId = mongoose.Schema.Types.ObjectId
-const REQUIRED_VALIDATION_MESSAGE = '{PATH} is required}'
+const REQUIRED_VALIDATION_MESSAGE = '{PATH} is required'
 
 let threadSchema = new mongoose.Schema({
   title: {type: String, required: REQUIRED_VALIDATION_MESSAGE},
   description: {type: String},
   date: {type: Date, default: Date.now()},
-  answers: {type: ObjectId, required: REQUIRED_VALIDATION_MESSAGE, ref: 'Answers'}
+  answers: {type: ObjectId, ref: 'Answers'}
 })
 
 let Thread = mongoose.model('Thread', threadSchema)
