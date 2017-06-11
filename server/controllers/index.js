ca1e0285b46df24b948c868511409e6a2d656e21
pkindalov <panayotkindalov@gmail.com>
2017-06-11 13:05:22 +0300

    Added 2 new controllers  - threads and answers in index.js file

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,11 @@
 const home = require('./home-controller')
 const users = require('./users-controller')
 const threads = require('./threads-controller')
+const answers = require('./anwers-controller')
 
 module.exports = {
   home: home,
   users: users,
-  threads: threads
+  threads: threads,
+  answers: answers
 }
