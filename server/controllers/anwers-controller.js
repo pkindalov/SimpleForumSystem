bec3d9c068cc3d9c29ab238f410ebfd417c86b3c
pkindalov <panayotkindalov@gmail.com>
2017-06-15 10:57:56 +0300

    Updated answers-controller. Little changes in listAnswersByLatestDate.  Added 3 new methods - editAnswerGet, editAnswerPost and deletePost

--- server/controllers/anwers-controller.js
+++ server/controllers/anwers-controller.js
@@ -52,19 +52,19 @@ module.exports = {
               .then(thread => {
                 // console.log(thread)
 
-                let authorsNames = []
-                for (let i = 0; i < thread.length; i++) {
-                  authorsNames.push(thread[i].author.username)
-                }
+                // let authorsNames = []
+                // for (let i = 0; i < thread.length; i++) {
+                //   authorsNames.push(thread[i].author.username)
+                // }
 
-                console.log(authorsNames)
+                // console.log(authorsNames)
                 // User
                 //       .findById()
                 // console.log(thread[0].author._id)
                 // console.log(thread[0].author.username)
                 res.render('answers/list', {
-                  threads: thread,
-                  authorsNames: authorsNames
+                  threads: thread
+                  // authorsNames: authorsNames
                 })
               })
     // Thread
@@ -81,6 +81,51 @@ module.exports = {
     //                             })
     //                           })
     //               })
+  },
+
+  editAnswerGet: (req, res) => {
+    let answerId = req.params.id
+
+    Answer
+            .findById(answerId)
+            .then(answer => {
+              console.log(answer)
+              res.render('answers/edit', {
+                answer: answer,
+                id: answerId
+              })
+            })
+  },
+
+  editAnswerPost: (req, res) => {
+    let updateAnswer = req.body
+    let answerId = req.params.id
+    let userId = req.user.id
+
+    Answer.findById(answerId, function (err, data) {
+      if (err) {
+        console.log(err)
+        return
+      }
+
+      data.answer = updateAnswer.answer
+      data.date = Date.now()
+      data.author = userId
+      data.save()
+    })
+    .then(
+      res.redirect('/threads/all')
+    )
+  },
+
+  deletePost: (req, res) => {
+    let answerId = req.params.id
+
+    Answer
+                .findByIdAndRemove(answerId)
+                .then(answer => {
+                  res.redirect('/threads/all')
+                })
   }
 
 }
